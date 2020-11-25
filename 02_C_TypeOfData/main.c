c0e8490233d488813f83a0bb282b81a7845e4e9a
Hulk7520 <1772666953@qq.com>
2020-11-25 14:42:18 +0800

    Update main.c

--- 02_C_TypeOfData/main.c
+++ 02_C_TypeOfData/main.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-	printf("%d\n", sizeof(char));
-	printf("%d\n", sizeof(short));
-	printf("%d\n", sizeof(int));
-	printf("%d\n", sizeof(float));
-	printf("%d\n", sizeof(long));
-	printf("%d\n", sizeof(double));
-	printf("%d\n", sizeof(long long));
+	printf("char = %d 字节(byte)\n", sizeof(char));        //1
+	printf("short = %d 字节(byte)\n", sizeof(short));		 //2
+	printf("int = %d 字节(byte)\n", sizeof(int));         //4
+	printf("float = %d 字节(byte)\n", sizeof(float));       //4
+	printf("long = %d 字节(byte)\n", sizeof(long));        //4
+	printf("double = %d 字节(byte)\n", sizeof(double));      //8
+	printf("long long = %d 字节(byte)\n", sizeof(long long));   //8
 
 	return 0;
 }
